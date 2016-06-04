ee82cf10265cd7f2ef3cd6b560a101fde56fa466
Radosław Waśko <programistagd@gmail.com>
2016-06-04 18:35:04 +0200

    Working on game states and basic player management system.

--- main.cpp
+++ main.cpp
@@ -3,23 +3,85 @@
 #include "seasocks/StringUtil.h"
 #include "seasocks/WebSocket.h"
 #include "seasocks/util/Json.h"
+#include "json.hpp"
 #include <set>
+#include <string>
 
 using namespace std;
 using namespace seasocks;
+using namespace nlohmann;
 
-struct ChatHandler : WebSocket::Handler {
-    set<WebSocket *> connections;
+struct Message{
+	Message(){}
+	Message(string data){
+		json msg = json::parse(data);
+		name = msg["name"];
+		data = msg["data"];
+	}
+	string name;
+	json data;
+	string toData() const{
+		json msg;
+		msg["name"] = name;
+		msg["data"] = data;
+		return msg.dump();
+	}
+};
+
+struct Player{
+	Player(WebSocket* ws) : ws(ws) {}
+	Player(){}
+	string nickname;
+	WebSocket* ws;
+};
+
+struct Game : WebSocket::Handler {
+	//helper functions
     void onConnect(WebSocket *socket) override 
-    { connections.insert(socket); }
+    {
+		connections.insert(make_pair(socket,Player(socket)));
+	}
     void onData(WebSocket *, const char *data) override 
-    { for (auto c : connections) c->send(data); }
+    {
+		Message m(data);
+	}
     void onDisconnect(WebSocket *socket) override 
-    { connections.erase(socket); }
+    {
+		if(state == WAITING)
+			connections.erase(socket);
+		else{
+			connections[socket].nickname = connections[socket].nickname + " (disconnected)";
+			connections[socket].ws = nullptr;
+		}
+	}
+	
+	void broadcast(const Message& m){
+		string data = m.toData();
+		for (auto c : connections){
+			if(c.second.ws!=nullptr)
+				c.second.ws->send(data.c_str());
+		}
+	}
+	void send(WebSocket* c, const Message& m){
+		if(c==nullptr) return;
+		c->send(m.toData().c_str());
+	}
+	
+	//logic implementation
+	map<WebSocket*,Player> connections;
+    enum State{
+		WAITING, PLAYING
+	} state = WAITING;
+    Game(){
+		
+	}
+	void refreshPlayers(){
+		
+	}
 };
 
 int main() {
     Server server(make_shared<PrintfLogger>());
-    server.addWebSocketHandler("/chat", make_shared<ChatHandler>());
+    server.addWebSocketHandler("/ws", make_shared<Game>());
     server.serve("../web", 3000);
 }
